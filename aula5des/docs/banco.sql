56a186df92c75e095facdf198b13fe67edd6e835
Maria Eduarda <157847827+dud4silvaa@users.noreply.github.com>
2024-09-02 08:08:06 -0300

    Add files via upload

---
+++ aula5des/docs/banco.sql
@@ -0,0 +1,12 @@
+DROP DATABASE cantinasesi;
+CREATE DATABASE cantinasesi;
+use cantinasesi;
+
+CREATE TABLE produtos (
+    id integer AUTO_INCREMENT PRIMARY KEY,
+    nomeProduto VARCHAR(255) NOT NULL,
+    nomeFornecedor VARCHAR(255) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    precoproduto FLOAT(20) NOT NULL,
+    custoproduto FLOAT(20) NOT NULL
+);
